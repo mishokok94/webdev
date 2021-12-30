80f298b2fea423e13d488684de15ee9eabedf2cb
mikle <example@gmail.com>
2021-12-30 15:40:38 +0200

    level 6

--- js/script.js
+++ js/script.js
@@ -52,7 +52,17 @@ for (let i = 0; i < 2; i++ ) {
 
         }
 
-
+        if (personalMovieDB.count < 10 ) {
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
+                console.log("Вы классический зритель");
+            }    else if (personalMovieDB.count >= 30){
+                console.log("Вы киноман");
+            } else {
+                console.log("Произошла ошибка");
+            }
+     
+        console.log(personalMovieDB);
 
 /* Задание на урок:
 
@@ -71,3 +81,5 @@ for (let i = 0; i < 2; i++ ) {
 
 
 // Код возьмите из предыдущего домашнего задания
+
+

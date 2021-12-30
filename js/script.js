798d28ba48c8f52d39061b59d91f1a8e3d10cde2
mikle <example@gmail.com>
2021-12-30 15:22:30 +0200

    level 5

--- js/script.js
+++ js/script.js
@@ -41,12 +41,18 @@ const personalMovieDB = {
 
 for (let i = 0; i < 2; i++ ) {
     const a = prompt('Один из последних просмотренных фильмов?', '' ),
-      b = prompt('На сколько оцените его?','');
+          b = prompt('На сколько оцените его?','');
+          if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+            personalMovieDB.movies[a] = b;
+            console.log(personalMovieDB);
+          } else {
+              console.log('error');
+              i--;
+          }
 
-      personalMovieDB.movies[a] = b;
-}
+        }
 
-console.log(personalMovieDB);
+
 
 /* Задание на урок:
 
